db4a9063ef7d9b719f7347b5929af2f2e0ba7e3d
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-05-02 15:22:55 +0100

    2nd tests passed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,12 +1,55 @@
 import Piece from './piece';
 import Square from '../square';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
 
-    getAvailableMoves(board) {
-        return new Array(new Square(1, 0), new Square(6, 7), new Square(4, 4)); 
+    getAvailableMoves(board) { //function to find the possible moves of the Piece(Pawn). Needs info from board.
+
+        //defines operator based on player colour
+        let playerOperator
+            if (this.player === Player.WHITE) {
+                playerOperator = (a, b) => {return a + b}
+            } else if (this.player === Player.BLACK) {
+                playerOperator = (a, b) => {return a - b}
+            }
+            else {
+                Console.log("Oops, there's been an error.")
+            }
+        let myPosition = board.findPiece(this); //defines current Position as the findPiece method of the board class, of 'this' piece
+        let nextSquare = new Square(playerOperator(myPosition.row, 1), myPosition.col);// defines next square of this piece, as current row + 1, and same column.
+        let nextSquareFirst = new Square(playerOperator(myPosition.row, 2), myPosition.col);
+        if(this.player === Player.WHITE) { //player.white is a field on player that defines white.
+            // this is a white piece
+            if (this.hasEverMoved === false) {
+                return new Array (nextSquareFirst, nextSquare);//???? allow new Square to be nextSquareAny or nextSquareFirst
+            } else if (this.hasEverMoved === true) {
+                return new Array (nextSquare); //???? allow new sqaure to be nextSquareAny// has not moved...
+            }
+                else {
+                    console.log ("Oops, there's been an error.")
+            }
+        } else { //assumes black
+            // this is a black piece
+            if (this.hasEverMoved === false) {
+                return new Array (nextSquareFirst, nextSquare);//???? allow new Square to be nextSquareAny or nextSquareFirst
+                } else if (this.hasEverMoved === true) {
+                return new Array (nextSquare); //???? allow new sqaure to be nextSquareAny// has not moved...
+                }
+                else {
+                console.log ("Oops, there's been an error.")
+                }
+
+        }
+
+
+        
+            
+
+        //return new Array(new Square(1, 0), new Square(6, 7), new Square(4, 4)); //defines new squares that the piece can move to.
     }
 }
+
--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,6 +1,7 @@
 export default class Piece {
     constructor(player) {
         this.player = player;
+        this.hasEverMoved = false; // starts out having never moved
     }
 
     getAvailableMoves(board) {
@@ -9,6 +10,8 @@ export default class Piece {
 
     moveTo(board, newSquare) {
         const currentSquare = board.findPiece(this);
+        // record that we have ever moved?
+        this.hasEverMoved = true; //  changes property of piece to say it has moved - this will remain true
         board.movePiece(currentSquare, newSquare);
     }
 }
