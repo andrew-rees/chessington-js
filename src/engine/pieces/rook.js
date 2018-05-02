3be3fc3f09039091451b39c925d56371eaabde9f
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-05-02 16:56:12 +0100

    rook movement tests pass

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,32 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let myPosition = board.findPiece(this);
+        //let nextSquares = new Square(myPosition.row /*??*/, myPosition.col /*??*/);
+
+        let possibleSquares = new Array();
+
+        for (let i = 0; i <= 7; i++) {// we want to define a new Square 8 times for a column
+            if (i !== myPosition.col) {//as long as we don't count our current column
+                possibleSquares.push(new Square(myPosition.row, i))//add a new object to the array on same row but with +1 in column
+            }
+            
+        }
+        for (let i = 0; i <= 7; i++) {// we want to define a new Square 8 times for a row
+            if (i !== myPosition.row) {//as long as we don't count our current row
+                possibleSquares.push(new Square(i, myPosition.col))//add a new object to the array on same column but with +1 in row
+            }
+
+            
+        }
+       return possibleSquares // the array of possible squares 
     }
+    
 }
+
+//1) define myPosition
+//2) define possibleSquares
+// 4) return new Array/new Square
+
+
+
