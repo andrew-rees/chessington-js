8abfacb8cee9fb8ab28232a7958fac13a02e0ebd
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-05-03 11:39:58 +0100

    knight movement fixed

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,44 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let myPosition = board.findPiece(this); //myPosition equals the place on this board where this findPiece places me right now
+        let possibleSquares = new Array();
+        
+        //R+1, C+2//
+        possibleSquares.push(new Square (myPosition.row+1, myPosition.col+2));
+
+        //R-1, C+2//
+        possibleSquares.push(new Square (myPosition.row-1, myPosition.col+2));
+
+        //R-1, C-2//
+        possibleSquares.push(new Square (myPosition.row-1, myPosition.col-2));
+
+        //R+1, C-2//
+        possibleSquares.push(new Square (myPosition.row+1, myPosition.col-2));
+
+        //R+2, C+1//
+        possibleSquares.push(new Square (myPosition.row+2, myPosition.col+1));
+
+        //R+2, C-1//
+        possibleSquares.push(new Square (myPosition.row+2, myPosition.col-1));
+
+        //R-2, C+1//
+        possibleSquares.push(new Square (myPosition.row-2, myPosition.col+1));
+
+        //R-2, C-1//
+        possibleSquares.push(new Square (myPosition.row-2, myPosition.col-1));
+
+
+
+
+
+
+
+
+
+
+
+
+        return possibleSquares;
     }
 }
