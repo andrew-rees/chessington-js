f20837e5f03f56b0bb97ae698be2a9bffa8cac18
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-05-03 11:50:44 +0100

    king movement fixed

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,26 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let myPosition = board.findPiece(this); //myPosition equals the place on this board where this findPiece places me right now
+        let possibleSquares = new Array();
+        //r+1, c+1
+        possibleSquares.push(new Square (myPosition.row+1, myPosition.col+1));
+        //r+1, c-1
+        possibleSquares.push(new Square (myPosition.row+1, myPosition.col-1));
+        //r-1, c+1
+        possibleSquares.push(new Square (myPosition.row-1, myPosition.col+1));
+        //r-1, c-1
+        possibleSquares.push(new Square (myPosition.row-1, myPosition.col-1));
+        //r = x, c+1
+        possibleSquares.push(new Square (myPosition.row, myPosition.col+1));
+        //r = x, c-1
+        possibleSquares.push(new Square (myPosition.row, myPosition.col-1));
+        //r+1, c=x
+        possibleSquares.push(new Square (myPosition.row+1, myPosition.col));
+        //r-1, c=x
+        possibleSquares.push(new Square (myPosition.row-1, myPosition.col));
+
+
+        return possibleSquares;
     }
 }
